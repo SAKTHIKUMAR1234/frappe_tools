ad413182cbfe5823d6186a618d569f1789833d93
Sakthi Kumar Pugalenthy <sakthi123msd@gmail.com>
2026-01-05 18:19:22 +0530

    document scanner report initialization

---
+++ frappe_tools/frappe_tools/report/scanned_document_detail_report/scanned_document_detail_report.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, sakthi123msd@gmail.com and contributors
+# For license information, please see license.txt
+
+# import frappe
+
+
+def execute(filters=None):
+	columns, data = [], []
+	return columns, data
